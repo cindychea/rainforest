7a90b7b0f4a39a00b570fe6ca19686e5318aed70
ynjacobs <ytj64611@gamil.com>
2019-05-30 17:01:43 -0400

    changed price to dollars

--- rainforest/rainforest/models.py
+++ rainforest/rainforest/models.py
@@ -5,6 +5,13 @@ class Product(models.Model):
     name = models.CharField(max_length = 255)
     description = models.TextField()
     price_in_cents = models.IntegerField(null=True)
+     
 
     def __str__(self):
         return self.name
+
+    def price_in_dollars(self):
+        dollars = self.price_in_cents / 100
+        return "${:.2f}".format(dollars)
+
+    
