c89b81e8a0d200b5d775e120f1877e395ef1e4e8
hassanashmawy <hassan.ashmawy@gmail.com>
2019-06-04 07:43:58 -0400

    1.2

--- rainforest/rainforest/urls.py
+++ rainforest/rainforest/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from rainforest.views import *
-
+ 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', root, name='home'),
